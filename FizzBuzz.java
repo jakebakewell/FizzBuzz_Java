e44199d6548f55a6d0f7d24fe824448c2ec1eab4
unknown <bakewelljake@gmail.com>
2021-04-06 08:38:31 -0500

    second commit

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,26 +1,23 @@
 public class FizzBuzz {
-    public static String fizzBuzz(int num) {
+    public FizzBuzz(){
+    }
+    public void fizzBuzz(int num) {
         for (int i = 1; i <= num; i++) {
             if (i % 15 == 0) {
                 System.out.println("FizzBuzz");
             }
-            if (i % 5 == 0) {
+            else if (i % 5 == 0) {
                 System.out.println("Buzz");
             }
-            if (i % 3 == 0) {
+            else if (i % 3 == 0) {
                 System.out.println("Fizz");
             }
-            if (i % 2 == 0) {
+            else if (i % 2 == 0) {
                 System.out.println("2");
             }
             else {
                 System.out.println(i);
             }
         }
-        String done = "Completed FizzBuzz";
-        return done;
-    }
-    public static void main(String[] args) {
-        fizzBuzz(150);
     }
 }
---
+++ FizzBuzzTest.java
@@ -0,0 +1,6 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz fizzy = new FizzBuzz();
+        fizzy.fizzBuzz(150);
+    }
+}
